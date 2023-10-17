12d4b5bb948338c33e791687635aae587a3a3257
Branislav Hozza <brankohozza@gmail.com>
2023-10-17 10:12:55 +0200

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace tp_backend.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Test"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
