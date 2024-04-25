d61958c965867e5e27f5b7e3b76710a1b30b0687
Samuel Hetteš <samuel.hettes@gmail.com>
2024-04-25 09:25:39 +0200

    :bug: Maybe fix second time

--- Database/Nodes/NFTNode.cs
+++ Database/Nodes/NFTNode.cs
@@ -51,7 +51,14 @@ namespace TAG.Database.Nodes
                     return new List<Attribute>();
                 }
 
-                return JsonConvert.DeserializeObject<List<Attribute>>(AttributesString) ?? new List<Attribute>();
+                try
+                {
+                    return JsonConvert.DeserializeObject<List<Attribute>>(AttributesString) ?? new List<Attribute>();
+                }
+                catch
+                {
+                    return new List<Attribute>();
+                }
             }
         }
 
